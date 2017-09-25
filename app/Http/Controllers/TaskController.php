c372379f71296cfd943c4b52ff8657462e772569
Someone <rgbjkbs@hdbf.ru>
2017-09-25 21:07:17 +0300

    Repositories/TaskRepository.php

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,28 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Repositories\TaskRepository;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 
 class TaskController extends Controller {
 
     /**
+     * Экземпляр TaskRepository.
+     *
+     * @var TaskRepository
+     */
+    protected $tasks;
+
+    /**
      * Создание нового экземпляра контроллера.
      *
      * @return void
      */
-    public function __construct() {
+    public function __construct(TaskRepository $tasks) {
         $this->middleware('auth');
+        $this->tasks = $tasks;
     }
-/**
- * 
- * @param Request $request
- * @return type
- */
-    
+
+    /**
+     * 
+     * @param Request $request
+     * @return type
+     */
     public function index(Request $request) {
-        return view('tasks.index');
+        $tasks = $request->user()->tasks()->get();
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    /**
+     * Создание новой задачи.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request) {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
     }
 
 }
