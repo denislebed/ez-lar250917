d8a3e0363be7c3b0a2f59e20c012c60384b8c326
Someone <rgbjkbs@hdbf.ru>
2017-09-25 20:24:40 +0300

    TasksController->index()

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,18 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
-class TaskController extends Controller
-{
-   /**
-   * Создание нового экземпляра контроллера.
-   *
-   * @return void
-   */
-  public function __construct()
-  {
-    $this->middleware('auth');
-  }
+class TaskController extends Controller {
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('auth');
+    }
+/**
+ * 
+ * @param Request $request
+ * @return type
+ */
+    
+    public function index(Request $request) {
+        return view('tasks.index');
+    }
+
 }
