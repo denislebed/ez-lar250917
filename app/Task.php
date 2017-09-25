480459a69d8d5a0fbfa80f2b11a3f80c36761904
Someone <rgbjkbs@hdbf.ru>
2017-09-25 20:01:44 +0300

    отношение Task->user

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
    protected $fillable = ['name'];
+
+   /**
+   * Получить пользователя - владельца данной задачи
+   */
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
